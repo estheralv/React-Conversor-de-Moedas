2f7538edab2ea30b8085afa34755958b67ac0a97
Esther <125416224+estheralv@users.noreply.github.com>
2024-12-10 19:38:46 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ConversordeMoedas from './assets/components/ConversordeMoedas'
+
+function App() {
+  return (
+    <>
+    <ConversordeMoedas/>
+    </>
+  )
+}
+
+export default App
